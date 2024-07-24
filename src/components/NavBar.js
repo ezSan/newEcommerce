73d83113a9b47ba78cbd02b8dbea9cb9175f481f
ezSan <ezequiel.sanchez.7391@gmail.com>
2024-07-24 11:00:51 -0300

    style(navBar): add logo & newStyle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { useRouter } from "next/router";
+import { useTheme } from "@mui/material/styles";
 
 export default function NavBar() {
+  const theme = useTheme();
   const router = useRouter();
+
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          <Button color="inherit" onClick={() => router.push("/")}>
-            Ez-Commerce
+      <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+        <Button color="inherit" onClick={() => router.push("/")}>
+          <Typography variant="h6">wildTech</Typography>
+        </Button>
+
+        <div>
+          <Button color="inherit" onClick={() => router.push("/Questions")}>
+            Preguntas frecuentes
           </Button>
-        </Typography>
 
-        <Button color="inherit" onClick={() => router.push("/Questions")}>
-          Preguntas frecuentes
-        </Button>
-
-        <Button color="inherit" onClick={() => router.push("/PanelAdmin")}>
-          Administrador
-        </Button>
+          <Button color="inherit" onClick={() => router.push("/PanelAdmin")}>
+            Administrador
+          </Button>
+        </div>
       </Toolbar>
     </AppBar>
   );
