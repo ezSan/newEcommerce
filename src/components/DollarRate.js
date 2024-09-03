58fe2b8e40d6488d218df5888dd4e668da4a2671
ezSan <ezequiel.sanchez.7391@gmail.com>
2024-09-03 15:51:16 -0300

    feat(header): add responsive to header & change name navBar to header

--- src/components/DollarRate.js
+++ src/components/DollarRate.js
@@ -1,33 +1,33 @@
-import React, { useEffect } from 'react';
-import { Box, Typography } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
-import { setDollarValue } from '../store/actions/dollarActions';
+import React, { useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { setDollarValue } from "../store/actions/dollarActions";
 
 const DollarRate = () => {
   const dispatch = useDispatch();
-  const rate = useSelector((state) => state.dollar.value);
+  const rate = useSelector(state => state.dollar.value);
 
-  useEffect(() => {
-    const fetchDollarRate = async () => {
-      try {
-        const response = await fetch('https://dolarapi.com/v1/dolares/blue');
-        const data = await response.json();
-        dispatch(setDollarValue(data.venta));
-      } catch (error) {
-        console.error('Error fetching dollar rate:', error);
-      }
-    };
+  useEffect(
+    () => {
+      const fetchDollarRate = async () => {
+        try {
+          const response = await fetch("https://dolarapi.com/v1/dolares/blue");
+          const data = await response.json();
+          dispatch(setDollarValue(data.venta));
+        } catch (error) {
+          console.error("Error fetching dollar rate:", error);
+        }
+      };
 
-    fetchDollarRate();
-  }, [dispatch]);
+      fetchDollarRate();
+    },
+    [dispatch]
+  );
 
   return (
-    <Box textAlign="right" sx={{ marginRight: '24px', mt: '6px' }}>
-      <Typography variant="body2" color="inherit">
-        Dólar hoy
-      </Typography>
-      <Typography variant="h5" color="inherit">
-        {rate !== null ? `$${rate}` : 'Cargando...'}
+    <Box textAlign="center">
+      <Typography variant="body1" color="inherit">
+        Dólar hoy {rate !== null ? `$${rate}` : "Cargando..."}
       </Typography>
     </Box>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,230 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Badge,
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  useMediaQuery,
+  MenuItem,
+  Menu,
+} from "@mui/material";
+import {
+  ShoppingCart as ShoppingCartIcon,
+  Menu as MenuIcon,
+  Dashboard as DashboardIcon,
+} from "@mui/icons-material";
+import { useRouter } from "next/router";
+import RegisterModal from "./RegisterModal";
+import LoginModal from "./LoginModal";
+import { useTheme } from "@mui/material/styles";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../store/actions/userActions";
+import CartModal from "./CartModal";
+import DollarRate from "./DollarRate";
+
+export default function Header() {
+  const theme = useTheme();
+  const router = useRouter();
+  const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const dispatch = useDispatch();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const user = useSelector((state) => state.user.user);
+  const cartItems = useSelector((state) => state.cart.items);
+
+  const handleOpenRegister = () => {
+    setIsRegisterOpen(true);
+  };
+
+  const handleCloseRegister = () => {
+    setIsRegisterOpen(false);
+  };
+
+  const handleOpenLogin = () => {
+    setIsLoginOpen(true);
+  };
+
+  const handleCloseLogin = () => {
+    setIsLoginOpen(false);
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    handleDrawerToggle(); // Cierra el drawer al cerrar sesión
+  };
+
+  const handleOpenCart = () => {
+    setIsCartOpen(true);
+  };
+
+  const handleCloseCart = () => {
+    setIsCartOpen(false);
+  };
+
+  const handleDrawerToggle = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const totalItems = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+
+  const renderNavButtons = () => (
+    <Box display="flex" alignItems="center">
+      {/* Botones de Sesión */}
+      {!user && (
+        <Button color="inherit" onClick={handleOpenLogin}>
+          Iniciar sesión
+        </Button>
+      )}
+      {user && (
+        <IconButton color="inherit" onClick={handleMenuClick}>
+          <MenuIcon />
+        </IconButton>
+      )}
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        keepMounted
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={Boolean(anchorEl)}
+        onClose={handleCloseMenu}
+      >
+        {user?.isAdmin && (
+          <MenuItem
+            onClick={() => {
+              handleCloseMenu();
+              router.push("/PanelAdmin");
+            }}
+          >
+            <DashboardIcon style={{ marginRight: "10px" }} /> Panel Admin
+          </MenuItem>
+        )}
+        {user ? (
+          <MenuItem onClick={handleLogout}>
+            <Typography color="error">Cerrar sesión</Typography>
+          </MenuItem>
+        ) : (
+          <MenuItem onClick={handleOpenLogin}>Iniciar sesión</MenuItem>
+        )}
+      </Menu>
+    </Box>
+  );
+
+  return (
+    <>
+      <header>
+       <AppBar position="static">
+          <Toolbar style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            {/* Sección Izquierda: Logo */}
+            <Button color="inherit" onClick={() => router.push("/")}>
+              <Typography variant="h6">wildTech</Typography>
+            </Button>
+
+            {/* Sección Derecha: Carrito y Menú */}
+            <Box display="flex" alignItems="center">
+              {/* Carrito Siempre Visible */}
+              <IconButton color="inherit" onClick={handleOpenCart}>
+                <Badge badgeContent={totalItems} color="error">
+                  <ShoppingCartIcon />
+                </Badge>
+              </IconButton>
+
+              {/* Menú Hamburguesa o Botones de Sesión */}
+              {isMobile ? (
+                <IconButton
+                  edge="end"
+                  color="inherit"
+                  aria-label="menu"
+                  onClick={handleDrawerToggle}
+                >
+                  <MenuIcon />
+                </IconButton>
+              ) : (
+                renderNavButtons()
+              )}
+            </Box>
+
+           
+            <Drawer
+              anchor="right"
+              open={isDrawerOpen}
+              onClose={handleDrawerToggle}
+            >
+              <List>
+                <ListItem button onClick={() => router.push("/")}>
+                  <ListItemText primary="Inicio" />
+                </ListItem>
+             
+                {user ? (
+                  <>
+                    {user.isAdmin && (
+                      <ListItem
+                        button
+                        onClick={() => {
+                          handleDrawerToggle();
+                          router.push("/PanelAdmin");
+                        }}
+                      >
+                        <DashboardIcon style={{ marginRight: "10px" }} /> Panel
+                        Admin
+                      </ListItem>
+                    )}
+                    <ListItem button onClick={handleLogout}>
+                      <ListItemText primary="Cerrar sesión" />
+                    </ListItem>
+                  </>
+                ) : (
+                  <>
+                  <ListItem button onClick={handleOpenRegister}>
+                    <ListItemText primary="Registrarse" />
+                  </ListItem>
+                  <ListItem button onClick={handleOpenLogin}>
+                    <ListItemText primary="Iniciar sesión" />
+                  </ListItem>
+                  </>
+                )}
+              </List>
+            </Drawer>
+          </Toolbar>
+          
+          <RegisterModal open={isRegisterOpen} onClose={handleCloseRegister} />
+          <LoginModal open={isLoginOpen} onClose={handleCloseLogin} />
+        </AppBar>
+      </header>
+
+      <Box
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          padding: "4px 16px",
+          borderBottom: `1px solid ${theme.palette.divider}`,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <DollarRate />
+      </Box>
+
+      {/* Modal del Carrito de Compras */}
+      <CartModal open={isCartOpen} onClose={handleCloseCart} />
+    </>
+  );
+}
